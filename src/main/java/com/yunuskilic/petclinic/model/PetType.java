a7f712310ebf995dfc77299155589208807b7a8c
Yunus Kılıç <yunskilic@gmail.com>
2018-09-02 18:54:00 +0300

    POJOs fixed

--- src/main/java/com/yunuskilic/petclinic/model/PetType.java
+++ src/main/java/com/yunuskilic/petclinic/model/PetType.java
@@ -2,4 +2,15 @@ package com.yunuskilic.petclinic.model;
 
 public class PetType {
     private String name;
+
+    public PetType() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
