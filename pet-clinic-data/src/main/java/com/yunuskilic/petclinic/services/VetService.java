661f99a627d3b9d6f095d80cf40285605023cc56
Yunus Kılıç <yunskilic@gmail.com>
2018-09-03 19:55:47 +0300

    CrudService added

--- pet-clinic-data/src/main/java/com/yunuskilic/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/yunuskilic/petclinic/services/VetService.java
@@ -2,12 +2,5 @@ package com.yunuskilic.petclinic.services;
 
 import com.yunuskilic.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
