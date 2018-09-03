6fdc107b0082e1d205fc506cd9758fea2f6c9a18
Yunus Kılıç <yunskilic@gmail.com>
2018-09-03 20:33:56 +0300

    owner controller and view added

---
+++ pet-clinic-web/src/main/java/com/yunuskilic/petclinic/Controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package com.yunuskilic.petclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners() {
+        return "owners/index";
+    }
+}
