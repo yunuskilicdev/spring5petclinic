eef632b8846f4b774b3344d8c549b8ed2eac1b91
Yunus Kılıç <yunskilic@gmail.com>
2018-09-14 20:37:00 +0300

    commandlinerunner added

--- pet-clinic-data/src/main/java/com/yunuskilic/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/yunuskilic/petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.yunuskilic.petclinic.services.map;
 
 import com.yunuskilic.petclinic.model.Owner;
-import com.yunuskilic.petclinic.services.CrudService;
+import com.yunuskilic.petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-web/src/main/java/com/yunuskilic/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/yunuskilic/petclinic/controller/VetController.java
@@ -1,4 +1,4 @@
-package com.yunuskilic.petclinic.Controllers;
+package com.yunuskilic.petclinic.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
