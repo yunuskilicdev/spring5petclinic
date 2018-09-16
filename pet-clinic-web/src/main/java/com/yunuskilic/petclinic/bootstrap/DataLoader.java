513c7a9cad48042869f80e1556f689f67a1b8b93
Yunus Kılıç <yunskilic@gmail.com>
2018-09-16 10:45:37 +0300

    jpa repos added

--- pet-clinic-web/src/main/java/com/yunuskilic/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/yunuskilic/petclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.yunuskilic.petclinic.bootstrap;
 
 import com.yunuskilic.petclinic.model.*;
-import com.yunuskilic.petclinic.service.OwnerService;
-import com.yunuskilic.petclinic.service.PetTypeService;
-import com.yunuskilic.petclinic.service.SpecialityService;
-import com.yunuskilic.petclinic.service.VetService;
+import com.yunuskilic.petclinic.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -17,13 +14,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialityService specialityService;
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialityService specialityService) {
+                      SpecialityService specialityService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialityService = specialityService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -88,6 +87,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners....");
 
