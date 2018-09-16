df8af371ccef4668276ce6982e4a05d5ea092d83
Yunus Kılıç <yunskilic@gmail.com>
2018-09-16 10:08:48 +0300

    entities converted to jpa

--- pet-clinic-data/src/main/java/com/yunuskilic/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/yunuskilic/petclinic/model/Pet.java
@@ -1,15 +1,36 @@
 package com.yunuskilic.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public Pet() {
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     public String getName() {
